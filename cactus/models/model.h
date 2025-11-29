44601525f1920952213051c9af7a85f96e94e7f1
harshaljanjani <harshaljanjani@gmail.com>
2025-11-29 17:07:48 +0530

    feat: Add Microsoft Phi-3
    Signed-off-by: harshaljanjani <harshaljanjani@gmail.com>

--- cactus/models/model.h
+++ cactus/models/model.h
@@ -138,6 +138,47 @@ protected:
 };
 
 
+class Phi3Model : public Model {
+public:
+    Phi3Model();
+    explicit Phi3Model(const Config& config);
+    ~Phi3Model() override = default;
+
+protected:
+    size_t build_attention(CactusGraph* gb, size_t normalized_input, uint32_t layer_idx,
+                          ComputeBackend backend, bool use_cache = false, size_t position_offset = 0) override;
+
+    size_t build_mlp(CactusGraph* gb, size_t normalized_h, uint32_t layer_idx,
+                    ComputeBackend backend) const override;
+
+    size_t build_transformer_block(CactusGraph* gb, size_t hidden, uint32_t layer_idx,
+                                  ComputeBackend backend, bool use_cache = false, size_t position_offset = 0) override;
+
+    size_t forward(const std::vector<uint32_t>& tokens, bool use_cache = false) override;
+    void load_weights_to_graph(CactusGraph* gb) override;
+
+private:
+    struct WeightNodeIDs {
+        size_t output_weight;
+        size_t output_norm_weight;
+
+        struct LayerWeights {
+            size_t attn_q_weight;
+            size_t attn_k_weight;
+            size_t attn_v_weight;
+            size_t attn_output_weight;
+            size_t input_layernorm_weight;
+            size_t ffn_gate_weight;
+            size_t ffn_up_weight;
+            size_t ffn_down_weight;
+            size_t post_attention_layernorm_weight;
+        };
+
+        std::vector<LayerWeights> layers;
+    } weight_nodes_;
+};
+
+
 class Siglip2VisionModel : public Model {
     friend class Lfm2VlModel;  
     
